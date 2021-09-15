bef4730b135382685f85f78a9ebaaf431d7fcfd8
Romil Shah <70903587+Rohil1321@users.noreply.github.com>
2021-09-15 19:19:48 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light" >
+      <img src="img/Logo.png" style="width: 75px;">
+      <a class="navbar-brand" href="index.php"><h3><b>KASHIE BANK</b></h3></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php"><h4>Home</h4></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php"><h4>Create User</h4></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php"><h4>Transfer Money</h4></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php"><h4>Transaction History</h4></a>
+              </li>
+          </div>
+       </nav>
